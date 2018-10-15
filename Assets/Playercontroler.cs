c4b832243bf443830e7155aec9349183a7980f36
Mr-CAD <44128918+Mr-CAD@users.noreply.github.com>
2018-10-15 10:13:13 -0400

    YES!!!

---
+++ Assets/Playercontroler.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroler : MonoBehaviour
+{
+    public float speedX;
+    public float speedY;
+    private int count;
+
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+
+    }
+    void FixedUpdate()
+    {
+
+        float movementHorizontal = Input.GetAxis("Horizontal");
+        float movementVertical = Input.GetAxis("Vertical");
+
+        Vector3 movementx = new Vector3(movementHorizontal, 0.0f, 0.0f);
+        Vector3 movementy = new Vector3(0.0f, 0.0f, movementVertical);
+
+        rb.AddForce(movementx * speedX);
+        rb.AddForce(movementy * speedY);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+
+        }
+    }
+}
+    
+
+    
+        
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Timer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour
+{
+
+    public Text timerText;
+    private float startTime;
+    private bool finnished = false;
+
+ 
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+   
+    void Update()
+    {
+        if (finnished)
+            return;
+
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        timerText.text = minutes + ":" + seconds;
+    }
+
+    public void Finnish()
+    {
+        finnished = true;
+        timerText.color = Color.yellow;
+    }
+
+
+
+}
